ca405971c12b58bffa51737c14391084e63a3ef9
mikan <lemon09mikan@gmail.com>
2024-08-18 18:09:07 +0900

    commit

---
+++ src/main/kotlin/com/example/Main.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import com.example.ListName
+import com.example.ToDoItem
+import com.example.ToDoList
+import com.example.User
+import org.http4k.core.HttpHandler
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+fun main() {
+    val items = listOf("write chapter", "insert code", "draw diagrams")
+    val toDoList =
+        ToDoList(ListName("book"), items.map(::ToDoItem))
+    val lists = mapOf(User("uberto") to listOf(toDoList))
+
+    val app: HttpHandler = Zettai(lists)
+    app.asServer(Jetty(8080)).start() //starting the server
+
+    println("Server started at http://localhost:8080/todo/uberto/book")
+}
